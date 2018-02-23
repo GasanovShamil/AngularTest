85fd8afea19e67849c0840717360ef36b26119dd
Gasanov <shamil.gasanov@cgi.com>
2018-02-23 16:50:30 +0100

    routing

---
+++ src/app/services/message.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class MessageService {
+  messages: String[] = [];
+  add(message: String): void {
+    this.messages.push(message);
+  }
+
+  clear(): void {
+    this.messages = [];
+  }
+}
